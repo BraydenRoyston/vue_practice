cc3add86cd61be4c950a9caab168fbea989ebac9
BraydenRoyston <brayden.royston10@gmail.com>
2021-03-19 22:19:53 -0400

    section 5

---
+++ vue_course/5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+    },
+  },
+  // these are lifecycle hooks
+  beforeCreate() {
+    console.log('beforeCreate()');
+    // could send https request, could set timers, etc.
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted');
+  },
+});
+
+app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
