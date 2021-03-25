b67739e291fee3aa40576d1c32c5776425a7e935
BraydenRoyston <brayden.royston10@gmail.com>
2021-03-24 23:39:01 -0400

    section 9

---
+++ vue_course/9/cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <header v-if="$slots.header">
+            <slot name="header">
+                <!-- Default content -->
+                <!-- <h2>Default</h2> -->
+            </slot>
+        </header>
+        <!-- slots are for HTML code -->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    // special property provided by vue
+    mounted() {
+        console.log(this.$slots.header);
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 2rem auto;
+        max-width: 30rem;
+        border-radius: 12px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        padding: 1rem;
+    }
+    header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    }
+</style>
