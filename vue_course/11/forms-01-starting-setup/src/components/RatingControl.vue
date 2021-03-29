22d3a9b857e2c4d43b843815dfcf9832f5b2d166
BraydenRoyston <brayden.royston10@gmail.com>
2021-03-29 13:10:40 -0400

    section 11

---
+++ vue_course/11/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,68 @@
+<template>
+    <ul>
+        <li :class="{active: activeOption === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+        <li :class="{active: activeOption === 'average'}"><button type="button" @click="activate('average')">Average</button></li>
+        <li :class="{active: activeOption === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],
+    emits: ['update:model-value'],
+    // we change it to a computed property so that it continually gets
+    // updated as we submit the form
+    // data() {
+    //     return {
+    //         activeOption: this.modelValue,
+    //     };
+    // },
+    computed: {
+        activeOption() {
+            return this.modelValue;
+        }
+    },
+    methods: {
+        activate(option) {
+            this.activeOption = option;
+            console.log('hello');
+            console.log(this.activeOption);
+            this.$emit('update:modelValue', option);
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0;
+    display: flex;
+
+}
+
+li {
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button {
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+
+.active {
+    border-color: purple;
+
+}
+.active button {
+    color: purple;
+}
+</style>
